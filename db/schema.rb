57fde8b9caa41532cde474ddf38acdedf07d3372
Alix Palmer <alix.palmer@hotmail.com>
2019-01-03 16:11:11 +1100

    Devise - don't forget to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_014704) do
+ActiveRecord::Schema.define(version: 2019_01_03_043112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,8 @@ ActiveRecord::Schema.define(version: 2019_01_03_014704) do
     t.integer "tenant_id"
     t.string "status"
     t.string "address"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "room_item_comment_photos", force: :cascade do |t|
@@ -61,14 +59,17 @@ ActiveRecord::Schema.define(version: 2019_01_03_014704) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reports", "users"
   add_foreign_key "room_item_comment_photos", "room_item_comments"
   add_foreign_key "room_item_comments", "room_items"
   add_foreign_key "room_items", "rooms"
